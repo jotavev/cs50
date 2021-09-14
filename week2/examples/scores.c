37ab54d93fca3f473bee43595b12697e5a62a015
jorge <juniorvalente2014@gmail.com>
2021-09-14 13:25:03 -0300

    caesar

--- week2/examples/scores.c
+++ week2/examples/scores.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <cs50.h>
 
+const int TOTAL = 3;
+
+float average(int length, int array[])
+{
+    int sum = 0;
+    for(int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    return sum / (float) length;
+}
 int main(void)
 {
-    int scores[3];
+    //int TOTAL = get_int("Total number of scores: ");
+    int scores[TOTAL];
     
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < TOTAL; i++)
     {
         scores[i] = get_int("Score: ");
     }
 
-    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
+    printf("Average: %f\n", average(TOTAL, scores));
 }
+
---
+++ week2/pset/caesar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char text[3];
+
+    text[0] = 'H';
+    text[1] = 'I';
+    text[2] = '!';
+
+    for(int i = 0; i < 3; i++)
+    {
+        printf("%c\n", text[i] + 1);
+    }
+
+}
