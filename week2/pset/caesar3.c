49edf371539e643117cab5f1541509ca9d938e7c
jorge <juniorvalente2014@gmail.com>
2021-09-30 11:32:50 -0300

    argcLogicDONE

--- week2/pset/caesar3.c
+++ week2/pset/caesar3.c
@@ -9,41 +9,62 @@ int isDigits(char arr[])
     {
         if (arr[i] >= 48 && arr[i] <= 57)
         {
-            bool result = true;
+            result = true;
         }
         else
         {
-            bool result = false;
+            result = false;
         }
-        printf("%i", result);
     }
     return result;
 }
 
+int strToInt(char arr[])
+{
+    int lenght = strlen(arr);
+    if (lenght == 2)
+    {
+        int decimal = (arr[0] - 48) * 10;
+        int numeral = arr[1] - 48;
+        int result = decimal + numeral;
+        printf("%i", result);
+    }
+    else if (lenght == 1)
+    {
+        int numeral = arr[0] - 48;
+        int result = numeral;
+        printf("%i", result);
+    }
+
+    return 0;
+}
+
 int main(int argc, string argv[])
 {
-
-    if (argc == 2)
+    if (argc != 2)
     {
-        isDigits(argv[1]);
-        printf("Sucess\n");
-        int lenght = strlen(argv[1]);
-        if (lenght == 2)
-        {
-            int decimal = (argv[1][0] - 48) * 10;
-            int numeral = argv[1][1] - 48;
-            int result = decimal + numeral;
-            printf("%i", result);
-        }
-        else if (lenght == 1)
-        {
-            int numeral = argv[1][0] - 48;
-            int result = numeral;
-            printf("%i", result);
-        }
-    } 
-    else
-    {
-        printf("Usage: ./caesar key");
+        printf("Usage: ./caesar key\n");
+        return 1;
     }
+
+    printf("Sucess\n");
+
+    int pp = isDigits(argv[1]);
+
+    int lenght = strlen(argv[1]);
+    if (lenght == 2)
+    {
+        int decimal = (argv[1][0] - 48) * 10;
+        int numeral = argv[1][1] - 48;
+        int result = decimal + numeral;
+        printf("%i", result);
+    }
+    else if (lenght == 1)
+    {
+        int numeral = argv[1][0] - 48;
+        int result = numeral;
+        printf("%i", result);
+    }
+
+    printf("\n\n\n%i", pp);
 }
