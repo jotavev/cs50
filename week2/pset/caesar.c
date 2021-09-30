bf08d4fd9a2b8760e8f38dd469caf9b6090c2491
jorge <juniorvalente2014@gmail.com>
2021-09-30 12:12:02 -0300

    userproof

--- week2/pset/caesar.c
+++ week2/pset/caesar.c
@@ -1,41 +1,72 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
-//int get_valid_number(arr[])
-//{
-//    int leght;
-//    do 
-//    {
-//        leght = get_string("")  
-//    }
-//}
-
-//converter string to int
-int strtoint(string arr[])
+int isDigits(char arr[])
 {
-    int decimal = (arr[1][0] - 48) * 10;
-    int numeral = arr[1][1] - 48;
-    int result = decimal + numeral;
+    int result;
+    for (int i = 0; arr[i] != '\0' ; i++)
+    {
+        if (arr[i] >= 48 && arr[i] <= 57)
+        {
+            result = 1;
+        }
+        else
+        {
+            result = 0;
+        }
+    }
+    return result;
+}
+
+int arraySize(char arr[])
+{
+    int counter = 0;
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        counter++;
+    }
+    return counter;
+}
+
+int strToInt(char arr[])
+{
+    int lenght = strlen(arr);
+    int result;
+    if (lenght == 2)
+    {
+        int decimal = (arr[0] - 48) * 10;
+        int numeral = arr[1] - 48;
+        result = decimal + numeral;
+    }
+    else if (lenght == 1)
+    {
+        int numeral = arr[0] - 48;
+        result = numeral;
+    }
     return result;
 }
 
 int main(int argc, string argv[])
 {
-    int cipher = strtoint(argv);
-    if (argc == 2)
+    int checkDigits = isDigits(argv[1]);
+
+    int arrayLenght = arraySize(argv[1]);
+
+    if (argc != 2 || checkDigits != 1 || arrayLenght > 2)
     {
-        printf("você passou 2 paremetros parabens\n");
-    } 
-    else
-    {
-        printf("não passou 2 parametros");
+        printf("Usage: ./caesar key\n");
+        return 1;
     }
-    printf("%i", cipher);
+
+    printf("Sucess\n");
+
+    int cipher = strToInt(argv[1]);
+
+
+    printf("\n\nthe array size is %i", arrayLenght);
+
+    printf("\n\nthe cipher number is %i", cipher);
+
+    printf("\n\n\ncheckdigits is %i", checkDigits);
 }
-
-
-
-    //if (cipher >= 0 && cipher < 27)
-    //{
-     //   printf("\nta no fafa beto");
-    //}
