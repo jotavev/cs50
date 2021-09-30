7c94363b4fca9db98ce160112fe3105d6da32cc5
jorge <juniorvalente2014@gmail.com>
2021-09-30 10:58:30 -0300

    beforeFixArgcLogic

--- week2/pset/caesar3.c
+++ week2/pset/caesar3.c
@@ -2,37 +2,48 @@
 #include <cs50.h>
 #include <string.h>
 
-int cat_number(int arr[])
+int isDigits(char arr[])
 {
-    for (int i = 0; arr[i] != '\0'; i++)
+    bool result;
+    for (int i = 0; arr[i] != '\0' ; i++)
     {
-        printf("!");
+        if (arr[i] >= 48 && arr[i] <= 57)
+        {
+            bool result = true;
+        }
+        else
+        {
+            bool result = false;
+        }
+        printf("%i", result);
     }
-return 0;
+    return result;
 }
 
 int main(int argc, string argv[])
 {
-    int lenght = strlen(argv[1]);
-    if (lenght == 2)
-    {
-        int decimal = (argv[1][0] - 48) * 10;
-        int numeral = argv[1][1] - 48;
-        int result = decimal + numeral;
-        printf("%i", result);
-    }
-    else if (lenght == 1)
-    {
-        int numeral = argv[1][0] - 48;
-        int result = numeral;
-    }
 
     if (argc == 2)
     {
-        printf("você passou 2 paremetros parabens\n");
+        isDigits(argv[1]);
+        printf("Sucess\n");
+        int lenght = strlen(argv[1]);
+        if (lenght == 2)
+        {
+            int decimal = (argv[1][0] - 48) * 10;
+            int numeral = argv[1][1] - 48;
+            int result = decimal + numeral;
+            printf("%i", result);
+        }
+        else if (lenght == 1)
+        {
+            int numeral = argv[1][0] - 48;
+            int result = numeral;
+            printf("%i", result);
+        }
     } 
     else
     {
-        printf("não passou 2 parametros");
+        printf("Usage: ./caesar key");
     }
 }
